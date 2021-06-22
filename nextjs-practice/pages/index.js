47895f4f1c533d4b66f5892412c54cdb95d11a09
danny <pongdaniel17@gmail.com>
2021-06-22 13:05:27 -0400

    render data from json

--- nextjs-practice/pages/index.js
+++ nextjs-practice/pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import ArticleList from "../components/ArticleList"
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
@@ -9,7 +10,19 @@ export default function Home() {
         <meta name="keywords" content="web development, programming"/>
       </Head>
 
-      <h1>HELLO WORLD</h1>
+      <ArticleList articles={articles}/>
+
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
